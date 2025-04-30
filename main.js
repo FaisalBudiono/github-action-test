35124839cc159b6f8b471e9bd8c25630595d7f3b
Faisal Budiono <ucul.project@gmail.com>
2025-05-01 05:04:27 +0700

    somechanges

--- main.js
+++ main.js
@@ -2,8 +2,39 @@ const { Octokit } = require("@octokit/rest");
 
 const okto = new Octokit();
 
-const asd = await okto.rest.pulls.get({
-  issue_number: context.issue.number,
+const parent = await okto.rest.pulls.get({
+  // issue_number: context.issue.number,
+  // owner: context.repo.owner,
+  // repo: context.repo.repo,
+  pull_number: "asd",
+});
+
+const labels = parent.data.labels
+  .map((l) => l.name)
+  .filter((l) => l === "invalid");
+
+const create = await okto.rest.pulls.create({
   owner: context.repo.owner,
   repo: context.repo.repo,
+  title: "title",
+  body: "amazing",
+  base: "staging",
+  head: "target",
 });
+
+const modifyLabel = await okto.rest.issues.setLabels({
+  owner: context.repo.owner,
+  repo: context.repo.repo,
+  issue_number: 20,
+  labels: [],
+});
+
+const stagingPRs = await okto.rest.pulls.list({
+  owner: context.repo.owner,
+  repo: context.repo.repo,
+  state: "open",
+  head: prInfo.head,
+  base: "auto-pr/staging",
+});
+
+stagingPRs.data[0].number;
