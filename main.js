61b401490616b8020191e1fc1ab0b3687ad3d30a
Faisal Budiono <ucul.project@gmail.com>
2025-05-01 03:49:46 +0700

    fix

--- main.js
+++ main.js
@@ -2,10 +2,8 @@ const { Octokit } = require("@octokit/rest");
 
 const okto = new Octokit();
 
-const asd = await okto.rest.pulls.create({
+const asd = await okto.rest.pulls.get({
+  issue_number: context.issue.number,
   owner: context.repo.owner,
   repo: context.repo.repo,
-  title: "hohoho",
-  body: "body bou",
-  base: 
 });
